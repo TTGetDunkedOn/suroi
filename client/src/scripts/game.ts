cf0a2e1546cfe34c8a7b1fd57e497f2b0cc6fd54
Kaleb Henrie <144857207+TTGetDunkedOn@users.noreply.github.com>
2024-05-30 14:13:45 -0400

    Update game.ts
    I disabled single instance for a perfect loop hopefully

--- client/src/scripts/game.ts
+++ client/src/scripts/game.ts
@@ -214,7 +214,7 @@ export class Game {
 
         this.music = sound.add("menu_music", {
             url: `./audio/music/menu_music${this.console.getBuiltInCVar("cv_use_old_menu_music") ? "_old" : MODE.specialMenuMusic ? `_${MODE.idString}` : ""}.mp3`,
-            singleInstance: true,
+            singleInstance: false,
             preload: true,
             autoPlay: true,
             volume: this.console.getBuiltInCVar("cv_music_volume")
